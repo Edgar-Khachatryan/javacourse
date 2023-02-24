c371c567777ea0e5c1e1e254e491d9db0c7e1845
Edgar <khachatryanedgar97@gmail.com>
2023-02-25 01:19:27 +0400

    homeworkPrimitiveTypes

--- src/primitive_data_types/Main.java
+++ src/primitive_data_types/Main.java
@@ -3,118 +3,100 @@ package primitive_data_types.src;
 public class Main {
     public static void main(String[] args) {
 
-// TODO Task 1
-//        byte a = 1;
-//        byte b = 2;
-//        System.out.println(a);
-//        System.out.println(b);
-//        System.out.println(Math.max(a,b));
-// TODO Task 5        System.out.println(Math.min(a,b));
-
-// TODO Task 2
-//        short r = 1;
-//        short c = 2;
-//        System.out.println(a);
-//        System.out.println(c);
-//        System.out.println(Math.max(r,c));
-// TODO Task 5        System.out.println(Math.min(r,c));
-
-// TODO Task 3
-//        int e = 1;
-//        int f = 2;
-//        System.out.println(e);
-//        System.out.println(f);
-//        System.out.println(Math.max(e,f));
-// TODO Task 5        System.out.println(Math.min(e,f));
-
-// TODO Taks 4
-//        long g = 1;
-//        long h = 2;
-//        System.out.println(g);
-//        System.out.println(h);
-//        System.out.println(Math.max(g,h));
-// TODO Task 5        System.out.println(Math.min(g,h));
-
-//TODO Task 6
-//        float j = 12.5f;
-//        float k = 13.5f;
-//        System.out.println(j);
-//        System.out.println(k);
-
-//TODO Task 8
-//        System.out.println(Math.max(j, k));
-//        System.out.println(Math.min(j, k));
-
-//TODO Task 7
-//        double i = 13.5d;
-//        double m = 12.5d;
-//        System.out.println(i);
-//        System.out.println(m);
-
-//TODO Task 8
-//        System.out.println(Math.max(i, m));
-//        System.out.println(Math.min(i, m));
 
 
-//TODO Task 9
-//        double a = 34.567839023d; // more than 7 decimal digits
-//        float b = 12.345f;
-//        float c = 8923.1234857f;
-//        float d = 3456.091f;
 
-//TODO Task 10
-//        char n = 'a';
-//        char o = 'b';
-//        System.out.println(n);
-//        System.out.println(o);
+        byte a = 1;
+        byte b = 2;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(Math.max(a,b));
+
+    System.out.println(Math.min(a,b));
 
 
-//TODO Task 11
-//        int s = 77;
-//        System.out.println((char)s);
-//        int w = 109;
-//        System.out.println((char)w);
-//        char t = '\uFF19';
-//        System.out.println(t);
+        short r = 1;
+        short c = 2;
+        System.out.println(a);
+        System.out.println(c);
+        System.out.println(Math.max(r,c));
+//
+    System.out.println(Ma
 
-//TODO Task 12
-//        boolean p = false;
-//        boolean q = true;
-//        System.out.println(p);
-//        System.out.println(q);
+        int e = 1;
+        int f = 2;
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(Math.max(e,f));
+        System.out.println(Math.min(e,f));
 
-//TODO Task 13
-//        int year = 201l;   // data type should be long
-//        System.out.println(year);
+        long g = 1;
+        long h = 2;
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(Math.max(g,h));
+        System.out.println(Math.min(g,h));
 
-//TODO Task 14
-//        int aa = 7;
-//        long bb = aa;
-//        System.out.println(bb);
+        float j = 12.5f;
+        float k = 13.5f;
+        System.out.println(j);
+        System.out.println(k);
 
-//TODO Task 15
-//        int myInt = 7;
-//        double myDouble = myInt;
-//        System.out.println(myDouble);
+        System.out.println(Math.max(j, k));
+        System.out.println(Math.min(j, k));
 
-//TODO Task 16
-//        double dd = 9.78d;
-//        int ii = (int)dd;
-//        System.out.println(ii);
+        double i = 13.5d;
+        double m = 12.5d;
+        System.out.println(i);
+        System.out.println(m);
 
-//TODO Task 17
-//        int ff = 9999;
-//        byte gg = (byte) ff;
-//        System.out.println(gg);
+        System.out.println(Math.max(i, m));
+        System.out.println(Math.min(i, m));
 
-//TODO Task 18
-//        double max = 3123456789.0d;
-//        double d = 1000.00d;
-//        char i ='i'; char j = 'j'; char ij = (char) (i + j);
-//        int a = 8;
-//        long h;
-//        int n = 11;
-//        int m = n;
+
+        double a = 34.567839023d; // more than 7 decimal digits
+        float b = 12.345f;
+        float c = 8923.1234857f;
+        float d = 3456.091f;
+
+        char n = 'a';
+        char o = 'b';
+        System.out.println(n);
+        System.out.println(o);
+
+
+        int s = 77;
+        System.out.println((char)s);
+        int w = 109;
+        System.out.println((char)w);
+        char t = '\uFF19';
+        System.out.println(t);
+
+        boolean p = false;
+        boolean q = true;
+        System.out.println(p);
+        System.out.println(q);
+        int year = 201l;   // data type should be long
+        System.out.println(year);
+        int aa = 7;
+        long bb = aa;
+        System.out.println(bb);
+        int myInt = 7;
+        double myDouble = myInt;
+        System.out.println(myDouble);
+        double dd = 9.78d;
+        int ii = (int)dd;
+        System.out.println(ii);
+        int ff = 9999;
+        byte gg = (byte) ff;
+        System.out.println(gg);
+        double max = 3123456789.0d;
+        double d = 1000.00d;
+        char i ='i'; char j = 'j'; char ij = (char) (i + j);
+        int a = 8;
+        long h;
+        int n = 11;
+        int m = n;
 
     }
 
