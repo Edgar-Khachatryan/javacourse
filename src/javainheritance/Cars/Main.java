ba9506c44869dac2ab8579773468e81426a2bc29
Edgar <khachatryanedgar97@gmail.com>
2023-02-25 01:20:15 +0400

    homeworkInheritance

--- src/javainheritance/Cars/Main.java
+++ src/javainheritance/Cars/Main.java
@@ -2,6 +2,7 @@ package javainheritance.Cars;
 
 public class Main {
 
+
     public static void main(String[] args) {
         Car truck = new Truck("600kg",3,"Petron");
         truck.setPassengerCount(1);
