5406295d6f6732db67976f676f347a5d5821428b
Edgar <khachatryanedgar97@gmail.com>
2023-03-03 17:32:22 +0400

    HomeWorkInheritance

--- src/javainheritance/SuperSub/Sub.java
+++ src/javainheritance/SuperSub/Sub.java
@@ -2,6 +2,10 @@ package javainheritance.SuperSub;
 
 public class Sub extends Super {
     String message = "Hello SubClass class";
+
+    /**
+     * printing messages from Child and Parent Class
+     */
     public void printMessage(){
         System.out.println(super.message);
         System.out.println(message);
