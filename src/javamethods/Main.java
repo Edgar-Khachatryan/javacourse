3b712237a77ca06b5886474edf76678484d9b8ae
Edgar <khachatryanedgar97@gmail.com>
2023-03-04 19:34:56 +0400

    homeworkExercises

--- src/javamethods/Main.java
+++ src/javamethods/Main.java
@@ -1,104 +1,91 @@
-import java.sql.SQLOutput;
-import java.util.Random;
+package javamethods;
+
 import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
-        Scanner a =  new Scanner(System.in);
-        System.out.println("Input an integer ");
-        int c = a.nextInt();
-        Scanner b = new Scanner(System.in);
-        System.out.println("Input an another integer ");
-        int d = b.nextInt();
-        System.out.println(scanner(c,d));
+        returnZero(8,7);
+        hello();
+        bool();
+        chr();
+        flt();
+        recur();
+
     }
 
     /**
      * @param x
      * @param y
-     * @return
-     * returning 0
+     * @return returning 0
      */
 
-    public static int scanner(int x, int y) {
+    public static int returnZero(int x, int y) {
         return 0;
     }
-}
 
-    public class Main {
-        public static void main(String[] args) {
-            hello();
-
-        }
-        public static void hello(){
-            Scanner hi = new Scanner(System.in);
-            System.out.println("Enter smth here");
-            String hello = hi.nextLine();
-            return;
-        }
-}
+    /**
+     * printing string
+     */
 
 
-public class Main {
+    public static void hello() {
+        Scanner hi = new Scanner(System.in);
+        System.out.println("Enter smth here");
+        String hello = hi.nextLine();
+        return;
+    }
 
-    public static void  bool(){
+    /**
+     * entering boolean value
+     */
+
+    public static void bool() {
         Scanner bool1 = new Scanner(System.in);
         System.out.println("Enter True or False");
         boolean final1 = bool1.nextBoolean();
         System.out.println("Hello World !");
     }
 
-    public static void main(String[] args) {
-        bool();
+
+    /**
+     * priting char
+     * @return
+     */
+
+    public static char chr() {
+        Scanner tar = new Scanner(System.in);
+        System.out.println("Enter a character");
+        char tar1 = tar.next().charAt(0);
+        System.out.println(tar1);
+        return tar1;
     }
-}
-
-    public class Main {
-
-        public static char chr(){
-            Scanner tar = new Scanner(System.in);
-            System.out.println("Enter a character");
-            char tar1 = tar.next().charAt(0);
-            System.out.println(tar1);
-            return tar1;
-        }
-
-        public static void main(String[] args) {
-            chr();
-        }
-}
-
-    public class Main {
-
-        public static float flt(){
-            Scanner first = new Scanner(System.in);
-            System.out.println("Enter a first floating number");
-            float a = first.nextFloat();
-            Scanner second = new Scanner(System.in);
-            System.out.println("Enter second floating number");
-            float b = second.nextFloat();
-
-            return a;
-        }
-
-        public static void main(String[] args) {
-
-            System.out.println(flt());
-
-        }
-}
 
 
+    /**
+     * printing float
+     * @return
+     */
 
-    public class Main {
+    public static float flt() {
+        Scanner first = new Scanner(System.in);
+        System.out.println("Enter a first floating number");
+        float a = first.nextFloat();
+        Scanner second = new Scanner(System.in);
+        System.out.println("Enter second floating number");
+        float b = second.nextFloat();
 
-        static void recur(){
-            System.out.println("Recursia");
-            recur();
-        }
+        return a;
+    }
 
-        public static void main(String[] args) {
-            recur();
-        }
+    /**
+     * creating recursion
+     */
+
+
+    static void recur() {
+        System.out.println("Recursia");
+        recur();
+    }
+
 }
